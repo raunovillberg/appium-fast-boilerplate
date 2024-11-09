55a5fb5c20c4353179460aa44d81f8bf3f6e3985
Rauno Villberg <rauno.villberg@gmail.com>
2024-11-09 18:11:44 +0200

    Remove PlatformNotSupportedException
    Unused - needed to build before running tests!

--- src/main/java/exceptions/PlatformNotSupportException.java
+++
@@ -1,8 +0,0 @@
-package exceptions;
-
-@SuppressWarnings("unused") // Compilation fails without this class??
-public class PlatformNotSupportException extends Exception {
-    public PlatformNotSupportException(String message) {
-        super(message);
-    }
-}
