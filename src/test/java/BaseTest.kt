3f2654777f7410bd5100f5a72df05f7dee3a8585
Rauno Villberg <rauno.villberg@gmail.com>
2024-11-10 16:22:20 +0200

    Run Appium via BaseTest.setup
    No need to manually run it anymore.

--- src/test/java/BaseTest.kt
+++ src/test/java/BaseTest.kt
@@ -1,21 +1,35 @@
 import constants.Target
 import io.appium.java_client.AppiumDriver
 import io.appium.java_client.android.options.UiAutomator2Options
+import io.appium.java_client.service.local.AppiumDriverLocalService
 import org.openqa.selenium.remote.DesiredCapabilities
 import org.testng.ITestContext
+import org.testng.annotations.AfterClass
 import org.testng.annotations.AfterMethod
+import org.testng.annotations.BeforeClass
 import org.testng.annotations.BeforeMethod
 import java.net.URI
 
 open class BaseTest {
     private val target = Target.ANDROID
     protected lateinit var driver: AppiumDriver
+    protected lateinit var service: AppiumDriverLocalService
 
-    @BeforeMethod(alwaysRun = true)
+    @BeforeClass
     fun setup(context: ITestContext) {
         context.setAttribute("target", target)
         println("Test target: $target")
+        service = AppiumDriverLocalService.buildDefaultService()
+        service.start()
+    }
 
+    @AfterClass
+    fun teardown() {
+        service.stop()
+    }
+
+    @BeforeMethod(alwaysRun = true)
+    fun setupBeforeMethod() {
         val capabilities = when (target) {
             Target.ANDROID -> mapOf(
                 "platformName" to "android",
@@ -34,13 +48,14 @@ open class BaseTest {
                 "app" to "src/test/resources/TestApp.app"
             )
         }
+
         val url = URI("http://127.0.0.1:4723").toURL()
         val options = UiAutomator2Options(DesiredCapabilities(capabilities))
         driver = AppiumDriver(url, options)
     }
 
     @AfterMethod(alwaysRun = true)
-    fun teardown() {
+    fun teardownAfterMethod() {
         driver.quit()
     }
 }
